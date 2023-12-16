858dd0cb358818055459cd4de490e02cb2b6b90d
ConradWarren <130634784+ConradWarren@users.noreply.github.com>
2023-12-16 12:28:25 -0700

    Create day_16_part_2.cpp

---
+++ Day16/day_16_part_2.cpp
@@ -0,0 +1,78 @@
+#include <fstream?
+#include <set>
+#include <string>
+#include <vector>
+#include <queue>
+
+int Breadth_First_Search(std::vector<std::string>& grid, int y, int x, int y_direction, int x_direction) {
+
+	std::queue<std::vector<int>> node_queue;
+	std::vector<std::vector<std::set<std::pair<int, int>>>> cache(grid.size(), std::vector<std::set<std::pair<int, int>>>(grid[0].size()));
+
+	int count = 0;
+
+	node_queue.push({ y-y_direction,x-x_direction,y_direction,x_direction });
+
+	while (!node_queue.empty()){
+
+		y = node_queue.front()[0] + node_queue.front()[2];
+		x = node_queue.front()[1] + node_queue.front()[3];
+		y_direction = node_queue.front()[2];
+		x_direction = node_queue.front()[3];
+		node_queue.pop();
+
+		if (y < 0 || y >= grid.size() || x < 0 || x >= grid[0].size() || cache[y][x].find({ y_direction, x_direction }) != cache[y][x].end()) {
+			continue;
+		}
+
+		if (cache[y][x].empty()) count++;
+
+		cache[y][x].insert({ y_direction, x_direction });
+		
+		if (grid[y][x] == '.' || (y_direction == 0 && grid[y][x] == '-') || (x_direction == 0 && grid[y][x] == '|')) {
+			node_queue.push({ y, x, y_direction, x_direction });
+		} 
+		else if (grid[y][x] == '/') node_queue.push({ y, x, -x_direction, -y_direction });
+		
+		else if (grid[y][x] == '\\') node_queue.push({ y, x, x_direction, y_direction });
+		
+		else if (grid[y][x] == '-') {
+			node_queue.push({ y, x, 0, 1 });
+			node_queue.push({ y, x, 0, -1 });
+		}
+		else if (grid[y][x] == '|') {
+			node_queue.push({ y,x, 1, 0 });
+			node_queue.push({ y,x, -1, 0 });
+		}
+	}
+	return count;
+}
+
+int Count_Energized_Cells(const std::string& input_file_path) {
+
+	std::ifstream file(input_file_path);
+	std::vector<std::string> grid;
+	std::string line;
+	int result = 0;
+	int current = 0;
+
+	while (std::getline(file, line)){
+		grid.push_back(line);
+	}
+	
+	for (int i = 0; i < grid[0].size(); i++) {
+		current = Breadth_First_Search(grid, 0, i, 1, 0);
+		result = (result > current) ? result : current;
+		current = Breadth_First_Search(grid, grid.size() - 1, i, -1, 0);
+		result = (result > current) ? result : current;
+	}
+
+	for (int i = 0;i < grid.size(); i++) {
+		current = Breadth_First_Search(grid, i, 0, 0, 1);
+		result = (result > current) ? result : current;
+		current = Breadth_First_Search(grid, i, grid[0].size()-1, 0, -1);
+		result = (result > current) ? result : current;
+	}
+
+	return result;
+}
